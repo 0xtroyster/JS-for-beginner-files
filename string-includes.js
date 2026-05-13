2f81aa08a7d36b6155b77861938de97cb1f5f869
0xtroyster <12094659+0xtroyster@users.noreply.github.com>
2026-05-13 16:22:16 +0700

    Create string-includes.js

---
+++ string-includes.js
@@ -0,0 +1,3 @@
+const sentence = "I am practicing JavaScript every day.";
+const hasJavaScript = [sentence.includes(](https://sentence.includes()"JavaScript");
+console.log("Contains JavaScript:", hasJavaScript);
